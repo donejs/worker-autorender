a05597d71a47d850d674ead9170c6b679bce241d
Matthew Phillips <matthew@matthewphillips.info>
2015-07-23 10:54:47 -0400

    Return an importPromise for when in the window

--- src/window_template.js
+++ src/window_template.js
@@ -3,11 +3,11 @@ define([], function(){
 obj || (obj = {});
 var __t, __p = '';
 with (obj) {
-__p += 'define([\n	"@loader",\n	"worker-render/window"\n], function(loader, windowWorker){\n\n	var supportsWorkers = typeof Worker !== "undefined";\n\n	if(supportsWorkers){\n		var worker = new Worker(loader.stealURL+"?main=' +
+__p += 'define([\n	"@loader",\n	"worker-render/window"\n], function(loader, windowWorker){\n\n	var supportsWorkers = typeof Worker !== "undefined";\n	var isNode = typeof process === "object" &&\n		{}.toString.call(process) === "[object process]";a\n\n	// A promise for when we have imported the "real" autorender.\n	// This is so that server-side rendering can wait for it.\n	var importPromise;\n\n	if(supportsWorkers){\n		var worker = new Worker(loader.stealURL+"?main=' +
 ((__t = ( workerMain )) == null ? '' : __t) +
-'");\n		windowWorker.updateWith(worker);\n	} else {\n		loader.import("' +
+'");\n		windowWorker.updateWith(worker);\n	} else {\n		importPromise = loader.import("' +
 ((__t = ( main )) == null ? '' : __t) +
-'").then(function(app){\n			app.start();\n		});\n	}\n\n});\n';
+'");\n\n		if(!isNode) {\n			importPromise.then(function(app){\n				app.start();\n			});\n		}\n	}\n\n	return {\n		importPromise: importPromise\n	};\n\n});\n';
 
 }
 return __p
